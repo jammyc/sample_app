22bef703ba270406a9ff9c00e5dc4bc29dc5e474
Karthik <ckjammy@gmail.com>
2014-03-03 02:08:36 -0500

    changes to exercise

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,12 +20,11 @@ describe "after visiting another page" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
+
 describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+          valid_signin(user) 
       end
 
       it { should have_title(user.name) }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,8 +8,8 @@ describe "User pages" do
 
 describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
+   
+ before {visit user_path(user) }
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
@@ -39,14 +39,14 @@ before { valid_signup(user) }
   
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
-      
+     
 end
 describe "after saving the user" do
         before { click_button submit }
-        #let(:user) { User.find_by(email: 'user@example.com') }
-        it { should have_link('Sign out') }
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+    it { should have_link('Sign out') }
+    it { should have_title(user.name) }
+    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
