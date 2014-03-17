8d4ec4d40a56638480e3fd15cee2bf82193159eb
Karthik <ckjammy@gmail.com>
2014-03-16 22:00:49 -0400

    exercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -192,5 +192,27 @@ describe "and unfollowing" do
 
 #following ends
   end
+
+describe "relationship associations" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+      other_user.follow!(@user)
+    end
+
+    it "should destroy associated relationships" do
+      relationships = @user.relationships
+      @user.destroy
+      relationships.should be_empty
+    end
+
+    it "should destroy associated reverse relationships" do
+      reverse_relationships = @user.reverse_relationships
+      @user.destroy
+      reverse_relationships.should be_empty
+    end
+  end
+#relatioship ends
  end
 
